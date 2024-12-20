c0b365962138783cfa5be955f31d50109a186b4d
Kaweesi Samuel <158592803+SucciHack@users.noreply.github.com>
2024-12-20 14:30:44 +0300

    Updated placeholder

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function renderProducts(items){
     items.forEach((item)=>{
         const productHtml=`
                 <a href="index.html?slug=${item.slug}" class="product">
-                    <img src="${item.productThumbnail || "./images/imgPlaceholder.jpg"}" alt="" width="80px" height="80px">
+                    <img src="${item.productThumbnail}" alt="" width="80px" height="80px">
                     <div class="content">
                         <p class="line-clamp">${item.name}</p>
                         <p class="line-clamp">${item.productDetails}</p>                   
@@ -60,9 +60,9 @@ function renderProduct(product){
 
                     <div class="receipt">
                         <div class="one">
-                            <img src="${product.productImages[0]}" alt="image">
-                            <img src="${product.productImages[1]}" alt="image">
-                            <img src="${product.productImages[2]}" alt="image">
+                            <img src="${product.productImages[0] || "./images/imgPlaceholder.jpg"}" alt="image">
+                            <img src="${product.productImages[1] || "./images/imgPlaceholder.jpg"}" alt="image">
+                            <img src="${product.productImages[2] || "./images/imgPlaceholder.jpg"}" alt="image">
                         </div>
                     </div>
                 </div>
